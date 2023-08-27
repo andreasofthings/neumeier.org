772c9b9410e4adf25e0ed133a3ad043230d2d14e
Andreas Neumeier <andreas@neumeier.org>
2023-08-27 07:38:39 +0200

    Create cookieconsent-init.js

---
+++ static/cookieconsent-init.js
@@ -0,0 +1,11 @@
+ // obtain cookieconsent plugin
+var cc = initCookieConsent();
+
+// example logo
+var logo = '<img src="https://fontmeme.com/permalink/220805/3b90158d204fdd325173a5c7873e7cc1.png" alt="Logo" loading="lazy" style="margin-left: -4px; margin-bottom: -5px; height: 35px">';
+var cookie = '🍪';
+
+// run plugin with config object
+cc.run({
+    current_lang : 'en',
+});
