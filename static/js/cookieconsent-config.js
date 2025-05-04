52ec1113ff2d5386d14cebeeed2dda0e8a1882c6
Andreas <andreas.neumeier@sap.com>
2025-05-04 18:50:56 +0200

    Update cookieconsent-config.js

--- static/js/cookieconsent-config.js
+++ static/js/cookieconsent-config.js
@@ -1,49 +1,49 @@
 // import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.umd.js';
 // import 'https://pramari.de/static/cookieconsent/cookieconsent.3.0.1.umd.js';
-import 'https://pramari.de/static/cookieconsent/cookieconsent.3.1.0.umd.js';
+import "/js/cookieconsent.js";
 
 function updateConsent(cookie) {
-        console.log(cookie);
+  console.log(cookie);
 }
 
 CookieConsent.run({
-       onFirstConsent: ({cookie}) => {
-            updateConsent(cookie);
+  onFirstConsent: ({ cookie }) => {
+    updateConsent(cookie);
+  },
+  onConsentChange: ({ cookie }) => {
+    updateConsent(cookie);
+  },
+  onChange: ({ cookie }) => {
+    updateConsent(cookie);
+  },
+  guiOptions: {
+    consentModal: {
+      layout: "box wide",
+      position: "bottom right",
+      equalWeightButtons: false,
+      flipButtons: false,
     },
-    onConsentChange: ({cookie}) => {
-            updateConsent(cookie);
+    preferencesModal: {
+      layout: "bar wide",
+      position: "right",
+      equalWeightButtons: false,
+      flipButtons: true,
     },
-    onChange: ({cookie}) => {
-            updateConsent(cookie);
+  },
+  categories: {
+    necessary: {
+      readOnly: true,
     },
-    guiOptions: {
-        consentModal: {
-            layout: "box wide",
-            position: "bottom right",
-            equalWeightButtons: true,
-            flipButtons: false
-        },
-        preferencesModal: {
-            layout: "bar wide",
-            position: "right",
-            equalWeightButtons: false,
-            flipButtons: true
-        }
+    analytics: {
+      enabled: true,
     },
-    categories: {
-        necessary: {
-            readOnly: true
-        },
-        analytics: {
-            enabled: true,
-        }
+  },
+  language: {
+    default: "en",
+    autoDetect: "browser",
+    translations: {
+      en: "https://pramari.de/static/cookieconsent/en.json",
     },
-    language: {
-        default: "en",
-        autoDetect: "browser",
-        translations: {
-            'en': 'https://pramari.de/static/cookieconsent/en.json'
-        }
-    },
-    disablePageInteraction: true
+  },
+  disablePageInteraction: false,
 });
